b30c949c1afca9fd69027cbde73d671909a061ee
camilocardona-ing <camicarsu07@gmail.com>
2025-10-03 18:45:08 -0500

    este es el primer commit

---
+++ src/main/java/com/mycompany/herenciayneatbeans/HerenciayNeatbeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herenciayneatbeans;
+
+/**
+ *
+ * @author camilocardonasuarez
+ */
+public class HerenciayNeatbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
